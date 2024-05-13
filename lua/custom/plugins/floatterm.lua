404a4b831b73dc2e9378fb0f24ae989c9649e8bb
Matteo Gassend <mgassend@gmail.com>
2024-05-13 19:51:13 +0200

    add: initial config

---
+++ lua/custom/plugins/floatterm.lua
@@ -0,0 +1,21 @@
+return {
+	"voldikss/vim-floaterm",
+	keys = function()
+		print("custom/configs/floatterm.lua")
+		-- vim.keymap.set('n', '<leader>g', '<cmd>FloatermNew lazygit<CR>', { desc = 'LazyGit' })
+		-- vim.keymap.set('n', '<leader>t', '<cmd>FloatermNew lazydocker<CR>', { noremap = true, silent = true })
+		local _keys = {
+			{
+				"<leader>tg",
+				":FloatermNew lazygit<CR>",
+				desc = "[Terminal] LazyGit",
+			},
+			{
+				"<leader>ts",
+				":FloatermNew spotify_player<CR>",
+				desc = "[Terminal] Spotify TUI",
+			},
+		}
+		return _keys
+	end,
+}
