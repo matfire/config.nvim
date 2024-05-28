050d114c3029fb788ba7d3a7ed61c7c42efcd0da
Matteo Gassend <mgassend@gmail.com>
2024-05-28 14:15:23 +0200

    del: removed useless lines

--- lua/custom/plugins/floatterm.lua
+++ lua/custom/plugins/floatterm.lua
@@ -1,9 +1,6 @@
 return {
 	"voldikss/vim-floaterm",
 	keys = function()
-		print("custom/configs/floatterm.lua")
-		-- vim.keymap.set('n', '<leader>g', '<cmd>FloatermNew lazygit<CR>', { desc = 'LazyGit' })
-		-- vim.keymap.set('n', '<leader>t', '<cmd>FloatermNew lazydocker<CR>', { noremap = true, silent = true })
 		local _keys = {
 			{
 				"<leader>tg",
