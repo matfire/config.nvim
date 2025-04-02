7ed625de37425cf1aa73c47121b44bfe7dd8c6fb
Matteo Gassend <mgassend@gmail.com>
2025-04-02 21:58:37 +0200

    add: autopairs config

--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -1,7 +1,9 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-	config = true,
+	config = function()
+		require("custom.configs.autopairs")
+	end,
 	-- use opts = {} for passing setup options
 	-- this is equivalent to setup({}) function
 }
